ef0a7edfbf16132b12de703c82ff03d13759dc2f
Cadu <ceblpm@cin.ufpe.br>
2021-11-21 19:15:46 -0300

    (Feature/student-crud): create all methods for students service

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { Student } from './entities/student.entity';
@@ -7,34 +8,63 @@ import { Student } from './entities/student.entity';
 @Injectable()
 export class StudentsService {
   constructor(
-    private readonly prisma: PrismaService
+    private readonly prisma: PrismaService,
+    private readonly usersService: UsersService
   ) {}
 
   async create(createStudentDto: CreateStudentDto): Promise<Student> {
+    const { name, email, password, registration } = createStudentDto
+    
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    })
+
     const student = await this.prisma.student.create({
-      data: createStudentDto
+      data: {
+        userId: user.id,
+        registration
+      }
     })
 
     return student
   }
 
   async findAll(): Promise<Student[]> {
-    return this.prisma.student.findMany({});
+    return this.prisma.student.findMany({
+      include: {
+        user: true
+      }
+    });
   }
 
-  async findOne(id: string) {
-    return this.prisma.student.findUnique({ 
+  async findOne(id: string): Promise<Student> {
+    return this.prisma.student.findUnique({
+      include: {
+        user: true
+      }, 
       where: {
         id
     }});
   }
 
   async update(id: string, updateStudentDto: UpdateStudentDto): Promise<Student> {
+    const { registration, ...rest } = updateStudentDto
+
+    const studentInfo = await this.findOne(id)
+
+    await this.usersService.update(studentInfo.id, rest)
+    
     return this.prisma.student.update({
       where: {
         id
       },
-      data: updateStudentDto
+      data: { 
+        registration
+      }
     })
   }
 
