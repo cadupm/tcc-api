5df4735ef9178cb78fcc561ad7892ab21db62139
Cadu <ceblpm@cin.ufpe.br>
2021-11-23 15:46:49 -0300

    Bugfix(update-services): add property user for take properties in deep level

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,6 +1,9 @@
+import { User } from "src/users/entities/user.entity"
+
 export class Student {
     id: string
     userId: string
+    user?: User
     registration: string
     createdAt: Date
     updatedAt: Date
