ef9a0807406708918792482b3db3a49f74cd6757
Cadu <ceblpm@cin.ufpe.br>
2021-11-27 11:42:11 -0300

    Feature(review-crud): create preview review entity and schema at prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from './errors/all-exception.filter';
 import { FilesModule } from './files/files.module';
 import { SubmissionsModule } from './submissions/submissions.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, StudentsModule, TeachersModule, MentorshipsModule, FilesModule, SubmissionsModule],
+  imports: [PrismaModule, UsersModule, StudentsModule, TeachersModule, MentorshipsModule, FilesModule, SubmissionsModule, ReviewsModule],
   controllers: [],
   providers: [{
     provide: APP_FILTER,
---
+++ src/reviews/entities/review.entity.ts
@@ -0,0 +1,7 @@
+export class Review {
+    id: number
+    comment: string
+    submissionId: number
+    createdAt: Date
+    updatedAt: Date
+}
