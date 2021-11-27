cf48a53e03c4c23141f3871ba96f7b020eb5de24
Cadu <ceblpm@cin.ufpe.br>
2021-11-27 11:42:44 -0300

    Feature(review-crud): add relation with submission

--- src/submissions/entities/submission.entity.ts
+++ src/submissions/entities/submission.entity.ts
@@ -1,8 +1,11 @@
+import { Review } from "src/reviews/entities/review.entity"
+
 export class Submission {
     id: number
     description: string
     notReviewed: boolean
     mentorshipId: string
+    reviews: Review[]
     createdAt: Date
     updatedAt: Date
 }
