5ad567c1de29af0cc546606dda600323f51f7dd3
Cadu <ceblpm@cin.ufpe.br>
2021-11-29 17:22:58 -0300

    Feature(permission): introduce useGuards for AuthGuard with jwt and RolesGuard with roles decorator at mentorship controller

--- src/mentorships/mentorships.controller.ts
+++ src/mentorships/mentorships.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UseGuards, Req } from '@nestjs/common';
 import { MentorshipsService } from './mentorships.service';
 import { CreateMentorshipDto } from './dto/create-mentorship.dto';
 import { UpdateMentorshipDto } from './dto/update-mentorship.dto';
+import { Roles } from 'src/users/roles/roles.decorator';
+import { Role } from 'src/users/roles/roles.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/users/guards/roles.guard';
 
 @Controller('mentorships')
+@UseGuards(AuthGuard('jwt'))
 export class MentorshipsController {
   constructor(private readonly mentorshipsService: MentorshipsService) {}
 
   @Post()
+  @Roles(Role.Student)
+  @UseGuards(RolesGuard)
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createMentorshipDto: CreateMentorshipDto) {
     return this.mentorshipsService.create(createMentorshipDto);
@@ -26,6 +33,7 @@ export class MentorshipsController {
   }
 
   @Patch(':id')
+  @Roles(Role.Teacher)
   @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateMentorshipDto: UpdateMentorshipDto) {
     return this.mentorshipsService.update(id, updateMentorshipDto);
