6ada85842be75ab00bf496d8d0ea3beabe0370d8
Cadu <ceblpm@cin.ufpe.br>
2021-11-28 21:56:39 -0300

    Feature(authorization): implement auth methods at auth service file

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    let user: User;
+    try {
+      [user] = await this.usersService.findAll(email);
+      console.log(user)
+    } catch (error) {
+      return null;
+    }
+
+    const isPasswordValid = password === user.password
+    if (!isPasswordValid) return null;
+
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
