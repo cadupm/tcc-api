bd09da254f9f2ee43d7af3954b45c3d0eb7506e4
Cadu <ceblpm@cin.ufpe.br>
2021-11-28 21:57:23 -0300

    Feature(authorization): inject useGuards and authGuard with jwt decorator type

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
+@UseGuards(AuthGuard('jwt'))
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
