532ef3f08fbf89ca4825e59b3341c885cb7158e1
Cadu <ceblpm@cin.ufpe.br>
2021-12-02 17:36:37 -0300

    Feature(multer-config): do not have specifics end-points to upload/delete files

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
 import { FilesService } from './files.service';
-import { FilesController } from './files.controller';
 import { PrismaModule } from 'src/database/prisma/prisma.module';
 
 @Module({
   imports: [ConfigModule, PrismaModule],
-  controllers: [FilesController],
+  controllers: [],
   providers: [FilesService]
 })
 export class FilesModule {}
