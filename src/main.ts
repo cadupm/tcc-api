e49bc6e3ef03fc35648f41ae773a7b246e8b3fb2
Cadu <ceblpm@cin.ufpe.br>
2021-12-15 17:20:40 -0300

    (Hotfix/Develop): fix main file

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
 
-  await app.listen(process.env.API_PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
