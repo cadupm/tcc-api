97d92fd0549307ed969084d7bee689e44315f497
Cadu <ceblpm@cin.ufpe.br>
2021-12-20 11:30:12 -0300

    (Hotfix/Develop): change expiresIn from accessToken to two hours

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { PrismaModule } from 'src/database/prisma/prisma.module';
   imports: [PrismaModule, ConfigModule, UsersModule, PassportModule,
     JwtModule.register({
       privateKey: process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '900s'}
+      signOptions: { expiresIn: '7200s'}
   })],
   providers: [AuthService, LocalStrategy, JwtStrategy, UsersService], 
   controllers: [AuthController],
